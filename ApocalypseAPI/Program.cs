53e8acfd1a7cd69ab3c2dde1d45c7d7877c85ffc
Sevetar <Sevatar.Condemned@gmail.com>
2018-06-05 20:42:04 +1000

    PLaying around with dependencies for self-hosting

--- ApocalypseAPI/Program.cs
+++ ApocalypseAPI/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
 using Microsoft.AspNetCore.Server.HttpSys;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -13,14 +15,36 @@ namespace ApocalypseAPI
 {
     public class Program
     {
+        //public static void Main(string[] args)
+        //{
+        //    BuildWebHost(args).Run();
+        //}
+
+        //public static IWebHost BuildWebHost(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //    .UseKestrel()
+        //        .UseStartup<Startup>()
+        //        .Build();
+
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-        }
+            var exePath = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+            var directoryPath = Path.GetDirectoryName(exePath);
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(directoryPath)
                 .UseStartup<Startup>()
                 .Build();
+
+            if (Debugger.IsAttached || args.Contains("--debug"))
+            {
+                host.Run();
+            }
+            else
+            {
+                host.RunAsService();
+            }
+        }
     }
 }
