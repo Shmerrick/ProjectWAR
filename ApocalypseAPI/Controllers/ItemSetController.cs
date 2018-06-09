9a69f2e10f674b2e5bcd82b0530a7a7fd24f1abd
Sevetar <Sevatar.Condemned@gmail.com>
2018-06-09 14:00:52 +1000

    Auto stash before checking out "master"

---
+++ ApocalypseAPI/Controllers/ItemSetController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApocalypseAPI.Models;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using NLog;
+
+namespace ApocalypseAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/ItemSet")]
+    public class ItemSetController : Controller
+    {
+        private readonly IDbConnectionService _db;
+        private MySqlConnection dbConnection { get; set; }
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public ItemSetController(IDbConnectionService db)
+        {
+            _db = db;
+            dbConnection = new MySqlConnection(db.GetConnectionString());
+        }
+
+        [HttpGet]
+        public List<ItemSet> GetAll()
+        {
+            try
+            {
+                _logger.Debug($"calling getall");
+                return dbConnection.Query<ItemSet>( $"SELECT sets.Entry, sets.Name, sets.ClassId, c.ClassName, sets.ItemSetFullDescription FROM war_world.item_sets sets, war_world.classes c " +
+                                                    $"where c.ClassId = sets.ClassId " +
+                                                    $" order by sets.Name; ").ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(long id)
+        {
+            _logger.Debug($"calling get by id {id}");
+            var item =  dbConnection.Query<ItemSet>($"SELECT sets.Entry, sets.Name, sets.ClassId, c.ClassName, sets.ItemSetFullDescription FROM war_world.item_sets sets, war_world.classes c " +
+                                     $"where c.ClassId = sets.ClassId " +
+                                     $" and sets.Entry = {id}; ").ToList();
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return Ok(item);
+        }
+    }
+}
---
+++ ApocalypseAPI/Models/ItemSet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApocalypseAPI.Models
+{
+    public class ItemSet
+    {
+        public int Entry { get; set; }
+        public string Name { get; set; }
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+        public string ItemSetFullDescription { get; set; }
+
+    }
+}
