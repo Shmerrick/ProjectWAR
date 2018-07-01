f29f5b274a63ed1d7ce361cd6a07bfa698b172c1
Sevetar <Sevatar.Condemned@gmail.com>
2018-07-01 18:29:25 +1000

    Removed references to Apoc.API

--- Launcher/ApocLauncher.Designer.cs
+++ Launcher/ApocLauncher.Designer.cs
@@ -389,17 +389,17 @@ namespace Launcher
             this.bnConnectLocal.UseVisualStyleBackColor = false;
             this.bnConnectLocal.Visible = false;
             this.bnConnectLocal.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(578, 666);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(102, 45);
-            this.button1.TabIndex = 26;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Visible = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1Async);
+            //// 
+            //// button1
+            //// 
+            //this.button1.Location = new System.Drawing.Point(578, 666);
+            //this.button1.Name = "button1";
+            //this.button1.Size = new System.Drawing.Size(102, 45);
+            //this.button1.TabIndex = 26;
+            //this.button1.Text = "button1";
+            //this.button1.UseVisualStyleBackColor = true;
+            //this.button1.Visible = false;
+            //this.button1.Click += new System.EventHandler(this.button1_Click_1Async);
             // 
             // ApocLauncher
             // 
