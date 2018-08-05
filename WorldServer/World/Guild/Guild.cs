906afbb4bc76592f046a41b62e9efabe778c9d6a
Zaru <pat.andraschko@web.de>
2018-08-05 11:26:16 +0200

    Guild log extended into initialized flag

--- WorldServer/World/Guild/Guild.cs
+++ WorldServer/World/Guild/Guild.cs
@@ -2017,13 +2017,15 @@ namespace WorldServer
                 return;
 
 
-            if (plr.Name.Contains("arena") || plr.Name.Contains("poko") || plr.Name.Contains("bram") || plr.Name.Contains("zanu") || plr.Name.Contains("niffils") ||
-                plr.Name.Contains("ikdorf") || plr.Name.Contains("grimjob"))
+            if (plr.Name.Contains("arena") || plr.Name.Contains("poko") || plr.Name.Contains("bram") || plr.Name.Contains("zaru") || plr.Name.Contains("niffils") ||
+                plr.Name.Contains("ikdorf") || plr.Name.Contains("grimjob")
+				|| plr.GldInterface.GetGuildName().Contains("Afk"))
             {
                 var l_CurrentStack = new System.Diagnostics.StackTrace(true);
 
                 GuildLogger.Debug($"{plr.Name} {l_CurrentStack.ToString()}");
-            }
+				GuildLogger.Debug($"{plr.Name} Initialized= {plr.Initialized.ToString()}");
+			}
 
         }
 
