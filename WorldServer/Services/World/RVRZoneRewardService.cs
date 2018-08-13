a7842729c49dd345bc5327a33775886c438b1507
Sevetar <Sevatar.Condemned@gmail.com>
2018-08-13 16:59:27 +1000

    Added extra details to RewardDistributor

--- WorldServer/Services/World/RVRZoneRewardService.cs
+++ WorldServer/Services/World/RVRZoneRewardService.cs
@@ -38,4 +38,5 @@ namespace WorldServer.Services.World
         }
 
     }
+
 }
--- WorldServer/World/Battlefronts/Apocalypse/Loot/RewardDistributor.cs
+++ WorldServer/World/Battlefronts/Apocalypse/Loot/RewardDistributor.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
+using Common;
+using Common.Database.World.Battlefront;
+using WorldServer.Services.World;
 
 namespace WorldServer.World.Battlefronts.Apocalypse.Loot
 {
-    class RewardDistributor
+    public class RewardDistributor
     {
+        public List<RVRZoneLockReward> ZoneLockRewards { get; private set; }
+
+        public RewardDistributor(List<RVRZoneLockReward> zoneLockRewards)
+        {
+            ZoneLockRewards = zoneLockRewards;
+        }
+
+        public void Distribute(LootBagTypeDefinition lootBag)
+        {
+            
+        }
     }
 }
