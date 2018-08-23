7a093bf96da02b37e338e9537b7cec95f9413db5
Sevetar <Sevatar.Condemned@gmail.com>
2018-08-23 23:15:42 +1000

    Reversed order of bag rarity

--- WorldServer/World/Battlefronts/Apocalypse/Loot/LootBagRarity.cs
+++ WorldServer/World/Battlefronts/Apocalypse/Loot/LootBagRarity.cs
@@ -4,20 +4,15 @@ namespace WorldServer.World.Battlefronts.Apocalypse.Loot
 {
     public enum LootBagRarity
     {
-        [Description("9980")]
-        Gold,
-        [Description("9943")]
-        Purple,
-        [Description("9942")]
-        Blue,
+        [Description("9940")]
+        White,
         [Description("9941")]
         Green,
-        [Description("9940")]
-        White
-
-
-
+        [Description("9942")]
+        Blue,
+        [Description("9943")]
+        Purple,
+        [Description("9980")]
+        Gold
     }
-
-
 }
