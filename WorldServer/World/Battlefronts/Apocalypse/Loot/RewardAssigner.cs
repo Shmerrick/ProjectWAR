99f8c9cd9d8bc70792162563a66d41b398b739ee
Sevetar <Sevatar.Condemned@gmail.com>
2018-10-17 20:08:03 +1100

    Removed dropping of duplicate rewards on zone flip.

--- WorldServer/World/Battlefronts/Apocalypse/Loot/RewardAssigner.cs
+++ WorldServer/World/Battlefronts/Apocalypse/Loot/RewardAssigner.cs
@@ -1,10 +1,7 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FrameWork;
-using NLog;
 
 namespace WorldServer.World.Battlefronts.Apocalypse.Loot
 {
@@ -40,23 +37,33 @@ namespace WorldServer.World.Battlefronts.Apocalypse.Loot
 
             // Define the types of awards to give
             var lootBagDefinitions = new LootBagTypeDefinition().BuildLootBagTypeDefinitions(numberLootBags);
-            RewardLogger.Debug($"Number loot bags {lootBagDefinitions.Count}");
+            RewardLogger.Info($"Number loot bags {lootBagDefinitions.Count}");
+
+            var i = 0;
+
             foreach (var lootBagTypeDefinition in lootBagDefinitions)
             {
+                // If we run out of players break the loop
+                if (randomisedPlayerList.Count >= i)
+                    break;
+                var selectedPlayer = randomisedPlayerList[i];
+
                 try
                 {
-                    var selectedPlayer = randomisedPlayerList[StaticRandom.Instance.Next((eligiblePlayers.Count))];
-
                     RewardLogger.Debug($"Selected player {selectedPlayer} {eligiblePlayers.Count} for reward");
-
                     lootBagTypeDefinition.Assignee = selectedPlayer;
                 }
                 catch (Exception e)
                 {
                     RewardLogger.Warn($"{e.Message}");
                 }
+                
+                i++;
             }
+            //var selectedPlayer = randomisedPlayerList[StaticRandom.Instance.Next((eligiblePlayers.Count))];
+            //RewardLogger.Debug($"Selected player {selectedPlayer} {eligiblePlayers.Count} for reward");
+            //lootBagTypeDefinition.Assignee = selectedPlayer;
             return lootBagDefinitions;
         }
-    }
+}
 }
