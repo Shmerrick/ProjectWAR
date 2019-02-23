5a57bf5de4b857fe57ca3aaaed03f33e29a71643
Ikthaleon <Sevatar.Condemned@gmail.com>
2019-02-23 17:51:25 +1100

    Added keep spawn location table

---
+++ Common/Database/World/Battlefront/RVRProgression - Copy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FrameWork;
+
+namespace Common.Database.World.Battlefront
+{
+    // Fixed value of a character 
+    [DataTable(PreCache = false, TableName = "player_keep_spawn", DatabaseName = "World", BindMethod = EBindingMethod.StaticBound)]
+    [Serializable]
+    public class PlayerKeepSpawn : DataObject
+    {
+        [PrimaryKey]
+        public int KeepId { get; set; }
+
+        [DataElement(AllowDbNull = false)]
+        public int X { get; set; }
+
+        [DataElement(AllowDbNull = false)]
+        public int Y { get; set; }
+
+        [DataElement(AllowDbNull = false)]
+        public int Z { get; set; }
+
+        
+    }
+}
