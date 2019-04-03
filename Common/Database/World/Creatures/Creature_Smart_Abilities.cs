d6025945d412e1c1d65f666afdc4a46dd621de49
Sevetar <Sevatar.Condemned@gmail.com>
2019-04-04 09:45:40 +1100

    Add Creature Smart Abilities table

---
+++ Common/Database/World/Creatures/Creature_Smart_Abilities.cs
@@ -0,0 +1,47 @@
+﻿using FrameWork;
+using System;
+
+namespace Common.Database.World.Creatures
+{
+    [DataTable(PreCache = false, TableName = "creature_smart_abilities", DatabaseName = "World", BindMethod = EBindingMethod.StaticBound)]
+    [Serializable]
+    public class CreatureSmartAbilities : DataObject
+    {
+        [PrimaryKey(AutoIncrement = true)]
+        public int UniqueId { get; set; }
+
+        [PrimaryKey]
+        public int CreeatureTypeId { get; set; }
+
+        [PrimaryKey]
+        public int CreeatureSubTypeId { get; set; }
+
+
+        [DataElement(AllowDbNull = false)]  // Name of this ability
+        public string Name { get; set; }
+
+        [DataElement(AllowDbNull = true)] // Speech to emote on execution
+        public string Speech { get; set; }
+
+        [DataElement(AllowDbNull = false)]  // Function that must be true for this to activate
+        public string Condition { get; set; }
+
+        [DataElement(AllowDbNull = false)]  // Function to be called on execution
+        public string Execution { get; set; }
+
+        [DataElement(AllowDbNull = false)]  // Chance of execution if Condition is true
+        public int ExecuteChance { get; set; }
+
+        [DataElement(AllowDbNull = false)]  // How long to cool down this ability (0 = none)
+        public int CoolDown { get; set; }
+        
+        [DataElement(AllowDbNull = true)]  // Sounds to play
+        public string Sound { get; set; }
+
+      
+
+    }
+
+
+
+}
