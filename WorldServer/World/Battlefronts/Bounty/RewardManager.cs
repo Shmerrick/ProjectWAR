dbfebf21ff8a339248e745370cf455094badb471
Ikthaleon <Sevatar.Condemned@gmail.com>
2019-04-05 19:38:28 +1100

    Add crests rewards if player has duplicate gear

--- WorldServer/World/Battlefronts/Bounty/RewardManager.cs
+++ WorldServer/World/Battlefronts/Bounty/RewardManager.cs
@@ -613,6 +613,19 @@ namespace WorldServer.World.Battlefronts.Bounty
                     break;
                 }
             }
+
+            // If there were items, but player had them, give them some crests as a reward
+            if (availableGearDrops.Count() > 0)
+            {
+                victim.lootContainer = new LootContainer { Money = 1000 };
+                victim.lootContainer.LootInfo.Add(new LootInfo(ItemService.GetItem_Info(208470)));
+                victim.lootContainer.LootInfo.Add(new LootInfo(ItemService.GetItem_Info(208470)));
+                victim.lootContainer.LootInfo.Add(new LootInfo(ItemService.GetItem_Info(208470)));
+                victim.lootContainer.LootInfo.Add(new LootInfo(ItemService.GetItem_Info(208470)));
+                victim.lootContainer.LootInfo.Add(new LootInfo(ItemService.GetItem_Info(208470)));
+                if (victim.lootContainer != null)
+                    victim.SetLootable(true, killer);
+            }
         }
 
         private bool ItemExistsForPlayer(uint itemId, List<uint> playerItemList)
