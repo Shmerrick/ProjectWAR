5b936c32739c3527c22815417705882090c576fc
Sevetar <Sevatar.Condemned@gmail.com>
2019-05-08 17:19:28 +1000

    Added tracking tables for zonelock and player rewards

---
+++ Common/Database/World/Battlefront/PlayerKillRewardHistory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using FrameWork;
+
+namespace Common.Database.World.Battlefront
+{
+    // Fixed value of a character 
+    [DataTable(PreCache = false, TableName = "rvr_player_kill_reward_history", DatabaseName = "World", BindMethod = EBindingMethod.StaticBound)]
+    [Serializable]
+    public class PlayerKillRewardHistory : DataObject
+    {
+        [PrimaryKey]
+        public long LockId { get; set; }
+
+        [DataElement(AllowDbNull = false), PrimaryKey]
+        public int RegionId { get; set; }
+        
+        [DataElement(AllowDbNull = false)]
+        public DateTime Timestamp { get; set; }
+
+        [DataElement(AllowDbNull = false)]
+        public int Tier { get; set; }
+
+        [DataElement(AllowDbNull = false)]
+        public int KillerCharacterId { get; set; }
+        
+        [DataElement(AllowDbNull = false)]
+        public int VictimCharacterId { get; set; }
+
+        [DataElement(AllowDbNull = false)]
+        public int ItemId { get; set; }
+
+        [DataElement(AllowDbNull = false)]
+        public string ItemName { get; set; }
+
+        [DataElement(AllowDbNull = false)]
+        public string KillerCharacterName { get; set; }
+
+        [DataElement(AllowDbNull = false)]
+        public string VictimCharacterName { get; set; }
+
+    }
+}
---
+++ Common/Database/World/Battlefront/ZoneLockRewardHistory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FrameWork;
+
+namespace Common.Database.World.Battlefront
+{
+    // Fixed value of a character 
+    [DataTable(PreCache = false, TableName = "rvr_zone_lock_bag_reward_history", DatabaseName = "World", BindMethod = EBindingMethod.StaticBound)]
+    [Serializable]
+    public class ZoneLockRewardHistory : DataObject
+    {
+        [PrimaryKey]
+        public long LockId { get; set; }
+
+        [DataElement(AllowDbNull = false), PrimaryKey]
+        public int RegionId { get; set; }
+        
+        [DataElement(AllowDbNull = false)]
+        public DateTime Timestamp { get; set; }
+
+        [DataElement(AllowDbNull = false)]
+        public int Tier { get; set; }
+
+        [DataElement(AllowDbNull = false)]
+        public int LockingRealm { get; set; }
+        
+        [DataElement(AllowDbNull = false)]
+        public int CharacterId { get; set; }
+
+        [DataElement(AllowDbNull = false)]
+        public string CharacterName { get; set; }
+
+        [DataElement(AllowDbNull = false)]
+        public int BagRarity { get; set; }
+
+        [DataElement(AllowDbNull = false)]
+        public int Contribution { get; set; }
+
+
+    }
+}
