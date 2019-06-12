5a92da26e8744bf13df53ab84968388977efccad
Ikthaleon <Sevatar.Condemned@gmail.com>
2019-06-12 12:31:26 +1000

    Additional Logging for rewards.

--- WorldServer/World/Battlefronts/Keeps/KeepRewardManager.cs
+++ WorldServer/World/Battlefronts/Keeps/KeepRewardManager.cs
@@ -125,6 +125,8 @@ namespace WorldServer.World.Battlefronts.Keeps
             var totalRenown = 250 * keep.Tier + 80 * keep.Tier * objCount + keep.PlayersKilledInRange * 80; // Ik : Increased values here.
             var totalInfluence = 40 * keep.Tier + 20 * keep.Tier * objCount + keep.PlayersKilledInRange * keep.Tier * 6;
 
+            RewardLogger.Info($"Players killed in range {keep.PlayersKilledInRange} {totalXp} {totalRenown} {totalInfluence}");
+
             if (keep.PlayersKilledInRange < 4 * keep.Tier)
             {
                 battlePenalty = true;
@@ -132,6 +134,8 @@ namespace WorldServer.World.Battlefronts.Keeps
                 totalXp = (int)(totalXp * (0.25 + keep.PlayersKilledInRange / 40f * 0.75));
                 totalRenown = (int)(totalRenown * (0.25 + keep.PlayersKilledInRange / 40f * 0.75));
                 totalInfluence = (int)(totalInfluence * (0.25 + keep.PlayersKilledInRange / 40f * 0.75));
+
+                RewardLogger.Info($"{keep.PlayersKilledInRange} players killed in range - battle penalty - reduced rewards");
             }
 
             try
@@ -153,13 +157,13 @@ namespace WorldServer.World.Battlefronts.Keeps
 
                     var scaler = 0.5f;
 
-                    RewardLogger.Trace($"Player {player.Name} is in range");
+                    RewardLogger.Debug($"Player {player.Name} is in range");
 
                     // Player in range must be eligible
                     if (playersInRange.Any(x => x.CharacterId == eligibleCharacterId.Key))
                     {
                         scaler += 0.5f;
-                        RewardLogger.Trace($"Player {player.Name} is in range and eligible");
+                        RewardLogger.Debug($"Player {player.Name} is in range and eligible");
                     }
 
                     RewardLogger.Debug($"Player {player.Name} totalRenown = {totalRenown} scaler = {scaler}");
@@ -173,7 +177,7 @@ namespace WorldServer.World.Battlefronts.Keeps
                         if (keep.Info.Race == 2)
                         {
                             totalInfluence= (int)(totalInfluence* scaler*1.2f);
-                            RewardLogger.Trace($"Player {player.Name} +20% INF in keep");
+                            RewardLogger.Debug($"Player {player.Name} +20% INF in keep");
                         }
                     }
                     else
@@ -181,7 +185,7 @@ namespace WorldServer.World.Battlefronts.Keeps
                         if (player.Info.Race == keep.Info.Race)
                         {
                             totalInfluence= (int)(totalInfluence* scaler*1.2f);
-                            RewardLogger.Trace($"Player {player.Name} +20% INF in keep");
+                            RewardLogger.Debug($"Player {player.Name} +20% INF in keep");
                         }
                     }
                    
@@ -190,7 +194,7 @@ namespace WorldServer.World.Battlefronts.Keeps
                     {
                         player.QtsInterface.HandleEvent(Objective_Type.QUEST_CAPTURE_KEEP, keep.Info.KeepId, 1);
 
-                        RewardLogger.Trace($"Player {player.Name} is valid");
+                        RewardLogger.Debug($"Player {player.Name} is valid");
 
                         player.AddXp((uint)totalXp, false, false);
                         player.AddRenown((uint)totalRenown, false, RewardType.ZoneKeepCapture, keep.Info.Name);
