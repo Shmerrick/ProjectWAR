3a500fedb2a920aa76281aa256e118aaaf33a9ec
Derrick Mysels <derrick.mysels@gmail.com>
2020-04-29 07:40:32 -0400

    Save
    Just saving work.
    
    (cherry picked from commit b7a8b17857232b952eb74e71a16dabb2db0315a2)

---
+++ WorldServer/World/MythicAbility/AbilityComponentVariables.cs
@@ -0,0 +1,32 @@
+﻿using Common.Database.World.MythicAbility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldServer.World.MythicAbility
+{    public class AbilityComponentVariables
+    {
+        public ushort ID;
+        public string A00;
+        public string Values;
+        public ushort Multipliers;
+        public ushort ActivationDelay;
+        public ushort Duration;
+        public ushort Flags;
+        public ushort IconAlwaysVisible;
+        public ushort Operation;
+        public ushort Interval;
+        public ushort Radius;
+        public ushort ConeAngle;
+        public ushort FlightSpeed;
+        public ushort A15;
+        public ushort MaxTargets;
+        public ushort Description;
+
+        public AbilityComponentVariables()
+        { }
+
+    }
+}
---
+++ WorldServer/World/MythicAbility/AbilityVariables.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldServer.World.MythicAbility
+{
+    class AbilityVariables
+    {
+        public ushort ID;
+        public string Name;
+        public string Description;
+        public ushort Castime;
+        public ushort Cooldown;
+        public ushort TacticType;
+        public ushort AbilityType;
+        public ushort A20;
+        public ushort A24;
+        public ushort CareerLine;
+        public ushort A32;
+        public ushort Flags;
+        public ushort EffectID;
+        public ushort A44;
+        public ushort Range;
+        public ushort Angle;
+        public ushort MoraleCost;
+        public ushort ChannelInterval;
+        public ushort A54;
+        public ushort ScaleStatMult;
+        public ushort NumTacticSlots;
+        public ushort AP;
+        public ushort A61;
+        public ushort A62;
+        public ushort A63;
+        public ushort AbilityImprovementThreshold;
+        public ushort Specialization;
+        public ushort StanceOrder;
+        public ushort A68;
+        public ushort MinLevel;
+        public ushort A70;
+        public ushort A71;
+        public ushort A132;
+        public ushort A136;
+        public ushort A140;
+        public ushort A142C;
+        public ushort Disabled;
+        public ushort CanClickOff;
+        public ushort ScaleStat;
+        public ushort CantCrit;
+        public ushort MoraleLevel;
+        public ushort AttackType;
+        public ushort CounterAmount;
+        public ushort RangeMin;
+        public ushort EnemyTargetIgnoreLOS;
+        public ushort FriendlyTargetIgnoreLOS;
+        public ushort RangeMax;
+        public ushort UniqueGroup;
+        public ushort Faction;
+        public ushort UsableWithBuff;
+        public ushort AnimationDelay;
+        public ushort BinIndex;
+        public ushort SpellDamageType;
+        public ushort Toggle;
+        public ushort ToggleGroup;
+        public ushort PatcherFileID;
+        public ushort CreateBinData;
+
+    }
+
+}
