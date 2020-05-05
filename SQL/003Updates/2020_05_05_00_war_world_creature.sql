ee68a1bc56d5dda715ef036dc49d6992b3e041b4
cooler-SAI <cooler19860212@yahoo.com>
2020-05-05 20:00:02 +0300

    DB/Creature: added emotes,texts,vendors in Grimmenhagen Tavern

---
+++ SQL/003Updates/2020_05_05_00_war_world_creature.sql
@@ -0,0 +1,7 @@
+UPDATE `war_world`.`creature_protos` SET `VendorID` = '29' WHERE `Entry` = '27027'; 
+UPDATE `war_world`.`creature_spawns` SET `Emote` = '104' WHERE `Guid` = '314866'; 
+UPDATE `war_world`.`creature_spawns` SET `Emote` = '104' WHERE `Guid` = '314840'; 
+UPDATE `war_world`.`creature_spawns` SET `Emote` = '104' WHERE `Guid` = '314842'; 
+UPDATE `war_world`.`creature_spawns` SET `Emote` = '104' WHERE `Guid` = '317291'; 
+UPDATE `war_world`.`creature_spawns` SET `Emote` = '150' WHERE `Guid` = '314835'; 
+UPDATE `war_world`.`creature_texts` SET `Entry` = '99731' , `Text` = 'Hello, stranger! Welcome to Grimmenhagen Tavern. Take a sit, relax, drink something...' WHERE `Creature_texts_ID` = ''; 
