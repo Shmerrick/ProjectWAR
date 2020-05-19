c018fd4206a29d7e4d5651ac119af3db851f30cf
cooler-SAI <cooler19860212@yahoo.com>
2020-05-19 11:51:46 +0300

    Core/Movement: fix Random move by @wardev (TODO: edit time move), closed https://github.com/Shmerrick/ProjectWAR/issues/5

--- WorldServer/World/Objects/Player.cs
+++ WorldServer/World/Objects/Player.cs
@@ -1021,7 +1021,7 @@ namespace WorldServer.World.Objects
         }
 
         private void ForceCloseMobsToWander(int distance)
-        {
+         {
 
 
             // Simple random seed.
@@ -1035,7 +1035,6 @@ namespace WorldServer.World.Objects
                 && x.Spawn.Proto.VendorID == 0
                 && x.Spawn.Proto.LairBoss == false
                 && x.Spawn.Proto.Title == 0
-                && x.Spawn.Proto.Emote >= 0
                 && !(x is Keep_Creature)
                 && x.Spawn.Proto.FinishingQuests == null
                      && !(x is Pet)
@@ -1051,26 +1050,42 @@ namespace WorldServer.World.Objects
                     {
                         var returnHome = new Point3D(creature.Spawn.WorldX, creature.Spawn.WorldY, creature.Spawn.WorldZ);
                         //SendClientMessage($"Asking {creature.Name} to return home");
-                        creature.MvtInterface.SetBaseSpeed(50);
+                        creature.MvtInterface.SetBaseSpeed(50);//50
                         creature.MvtInterface.Move(returnHome);
-                        creature.MvtInterface.SetBaseSpeed(100);
+                        creature.MvtInterface.SetBaseSpeed(100);// 100
                     }
                     else
                     {
-                        var point = CalculatePoint(random, 200, creature.Spawn.WorldX, creature.Spawn.WorldY);
+                        var point = CalculatePoint(random, 100, creature.Spawn.WorldX, creature.Spawn.WorldY);//200
 
                         if (creature.LOSHit((ushort)ZoneId, new Point3D(point.X, point.Y, creature.Z)))
                         {
                             //SendClientMessage($"Asking {creature.Name} to move from {creature.Spawn.WorldX},{creature.Spawn.WorldY} to {point.X},{point.Y}");
-                            creature.MvtInterface.SetBaseSpeed(50);
+                            creature.MvtInterface.SetBaseSpeed(50);// 50
                             creature.MvtInterface.Move(point.X, point.Y, creature.Z);
-                            creature.MvtInterface.SetBaseSpeed(100);
+                            creature.MvtInterface.SetBaseSpeed(100);// 100
+
+
+                            if (creature.MvtInterface.MoveState == MovementInterface.EMoveState.None)
+                            {
+                                creature.MvtInterface.Move(point.X, point.Y, creature.Z);
+                            }
+
+                            if (creature.LOSHit((ushort)ZoneId, new Point3D(point.X, point.Y, creature.Z)))
+                            {
+                                var returnHome = new Point3D(creature.Spawn.WorldX, creature.Spawn.WorldY, creature.Spawn.WorldZ);
+
+                                creature.MvtInterface.Move(returnHome);
+
+                                creature.MvtInterface.SetBaseSpeed(50);// 100
+                         
+                            }
+
                         }
                     }
-
                 }
             }
-            //}
+            
         }
 
         public static bool BetweenRanges(int a, int b, int number)
