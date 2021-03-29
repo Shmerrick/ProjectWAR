80acf63a613718e147985bd4a271aa4a032f644a
cooler-SAI <cooler19860212@yahoo.com>
2021-03-29 19:22:16 +0300

    typo

--- SQL/002Updates/29-03-2021Temp land of the dead and dwalf city portals.sql
+++
@@ -1,3 +0,0 @@
-REPLACE INTO `gameobject_protos` VALUES ('66699895', 'Land of the dead Portal Order', '3460', '50', '1', '0', '100', null, null, '7682 0 18046 4 5 14553', '0', '0', '0', '0', '0', '0', null, '0', '0');
-REPLACE INTO `gameobject_protos` VALUES ('66699896', 'Portal to Dwalf City', '1675', '50', '1', '0', '1', null, null, '7682 0 0 4 0 0', '0', '0', '0', '0', '0', '0', null, '0', '0');
-REPLACE INTO `gameobject_protos` VALUES ('66699897', 'Land of the dead Portal Destro', '3460', '50', '1', '0', '1', null, null, '7682 0 0 4 0 0', '0', '0', '0', '0', '0', '0', null, '0', '0');
