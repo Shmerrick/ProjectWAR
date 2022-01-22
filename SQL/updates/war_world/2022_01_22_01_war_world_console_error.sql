16172b79fbe14c8601964667f3156bd1cafd8988
cooler-SAI <cooler19860212@yahoo.com>
2022-01-22 21:53:34 +0300

    Quick fix

--- SQL/updates/war_world/2022_01_22_01_war_world_console_error.sql
+++ SQL/updates/war_world/2022_01_22_01_war_world_console_error.sql
@@ -1,38 +1,45 @@
-UPDATE `war_world`.`ability_line_to_buff_type` SET `ID` = '0' WHERE `Line ID` = '1001'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `ID` = '0' WHERE `Line ID` = '1002'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `ID` = '0' WHERE `Line ID` = '1003'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `ID` = '0' WHERE `Line ID` = '1004'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `ID` = '0' WHERE `Line ID` = '1005'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `ID` = '0' WHERE `Line ID` = '1006'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `ID` = '0' WHERE `Line ID` = '1007'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `ID` = '0' WHERE `Line ID` = '1008'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `ClientSideEnumerationValue` = '0' , `BuffFrameRed` = '0' WHERE `Line ID` = '1001'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameRed` = '0' WHERE `Line ID` = '1002'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameRed` = '0' WHERE `Line ID` = '1003'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameRed` = '0' WHERE `Line ID` = '1004'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameRed` = '0' WHERE `Line ID` = '1005'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameRed` = '0' WHERE `Line ID` = '1006'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameRed` = '0' WHERE `Line ID` = '1007'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `ClientSideEnumerationValue` = '0' , `BuffFrameRed` = '0' WHERE `Line ID` = '1008'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `ClientSideEnumerationValue` = '0' WHERE `Line ID` = '1007'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `ClientSideEnumerationValue` = '0' WHERE `Line ID` = '1006'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `ClientSideEnumerationValue` = '0' WHERE `Line ID` = '1005'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `ClientSideEnumerationValue` = '0' WHERE `Line ID` = '1004'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `ClientSideEnumerationValue` = '0' WHERE `Line ID` = '1003'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `ClientSideEnumerationValue` = '0' WHERE `Line ID` = '1002'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameGreen` = '0' WHERE `Line ID` = '1001'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameGreen` = '0' WHERE `Line ID` = '1002'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameGreen` = '0' WHERE `Line ID` = '1003'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameGreen` = '0' WHERE `Line ID` = '1004'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameGreen` = '0' WHERE `Line ID` = '1005'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameGreen` = '0' WHERE `Line ID` = '1006'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameGreen` = '0' WHERE `Line ID` = '1007'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameGreen` = '0' , `BuffFrameBlue` = '0' WHERE `Line ID` = '1008'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameBlue` = '0' WHERE `Line ID` = '1007'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameBlue` = '0' WHERE `Line ID` = '1006'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameBlue` = '0' WHERE `Line ID` = '1005'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameBlue` = '0' WHERE `Line ID` = '1004'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameBlue` = '0' WHERE `Line ID` = '1003'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameBlue` = '0' WHERE `Line ID` = '1002'; 
-UPDATE `war_world`.`ability_line_to_buff_type` SET `BuffFrameBlue` = '0' WHERE `Line ID` = '1001'; 
-ALTER TABLE `war_world`.`ability_line_to_buff_type` CHANGE `Type Name` `Type Name` TEXT CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL, CHANGE `Client-side Enumeration Value` `Client-side Enumeration Value` INT(11) NOT NULL, CHANGE `Buff Frame Red` `Buff Frame Red` INT(11) NOT NULL, CHANGE `Buff Frame Green` `Buff Frame Green` INT(11) NOT NULL, CHANGE `Buff Frame Blue` `Buff Frame Blue` INT(11) NOT NULL, CHANGE `ID` `ID` INT(11) NOT NULL, CHANGE `TypeName` `TypeName` TEXT CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL, CHANGE `ClientSideEnumerationValue` `ClientSideEnumerationValue` INT(11) NOT NULL, CHANGE `BuffFrameRed` `BuffFrameRed` INT(11) NOT NULL, CHANGE `BuffFrameGreen` `BuffFrameGreen` INT(11) NOT NULL, CHANGE `BuffFrameBlue` `BuffFrameBlue` INT(11) NOT NULL; 
+DROP TABLE IF EXISTS `ability_line_to_buff_type`;
+
+CREATE TABLE `ability_line_to_buff_type` (
+  `ID` int(11) DEFAULT NULL,
+  `TypeName` text,
+  `ClientSideEnumerationValue` int(11) DEFAULT NULL,
+  `BuffFrameRed` int(11) DEFAULT NULL,
+  `BuffFrameGreen` int(11) DEFAULT NULL,
+  `BuffFrameBlue` int(11) DEFAULT NULL,
+  `ability_line_to_buff_type_ID` varchar(255) NOT NULL,
+  PRIMARY KEY (`ability_line_to_buff_type_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `ability_line_to_buff_type` */
+
+LOCK TABLES `ability_line_to_buff_type` WRITE;
+
+insert  into `ability_line_to_buff_type`(`ID`,`TypeName`,`ClientSideEnumerationValue`,`BuffFrameRed`,`BuffFrameGreen`,`BuffFrameBlue`,`ability_line_to_buff_type_ID`) values 
+(1001,'Hex',1,184,0,9,'1'),
+(1002,'Curse',2,130,0,197,'2'),
+(1003,'Cripple',3,0,167,12,'3'),
+(1004,'Ail',4,104,186,255,'4'),
+(1005,'Bolster',5,104,186,255,'5'),
+(1006,'Augment',6,104,186,255,'6'),
+(1007,'Bless',7,255,220,80,'7'),
+(1008,'Enchant',8,104,186,255,'8');
+
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `rvr_player_contribution`;
+
+CREATE TABLE `rvr_player_contribution` (
+  `Id` int(11) NOT NULL,
+  `CharacterId` int(10) unsigned NOT NULL,
+  `BattleFrontId` int(11) NOT NULL,
+  `ContributionSerialised` text NOT NULL,
+  `Timestamp` datetime NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `rvr_player_contribution` */
+
+LOCK TABLES `rvr_player_contribution` WRITE;
+
+UNLOCK TABLES;
