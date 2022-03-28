0297c47d409d35ebfb2448f5b1b75ded1eb9588f
cooler-SAI <cooler19860212@yahoo.com>
2022-03-28 15:10:17 +0300

    Core/Scripts: delete trash from AltdorfSewersWingIII.cs

--- Scripts/Scripts/Dungeons/AltdorfSewersWingIII.cs
+++ Scripts/Scripts/Dungeons/AltdorfSewersWingIII.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Common;
-using FrameWork;
 using WorldServer.Services.World;
 using WorldServer.World.Interfaces;
 using WorldServer.World.Objects;
