370f0ac8fcccbe90c8785c2dd8cf63fa0b4442b7
cooler-SAI <cooler19860212@yahoo.com>
2022-05-01 20:50:44 +0300

    DB/Wandering: enable move MONSTERS_CHAOS_BREEDS_TUSKGOR and ANIMALS_BEASTS_WOLF

---
+++ SQL/updates/war_world/2022_05_01_03_war_world_wandering.sql
@@ -0,0 +1,4 @@
+-- MONSTERS_CHAOS_BREEDS_TUSKGOR 
+UPDATE `creature_protos` SET `IsWandering` = 1 WHERE `CreatureType` = 19 AND `CreatureSubType` = 114;
+-- ANIMALS_BEASTS_WOLF
+UPDATE `creature_protos` SET `IsWandering` = 1 WHERE `CreatureType` = 1 AND `CreatureSubType` = 8;
