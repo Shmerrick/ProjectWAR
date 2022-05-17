cdc3826a5aa48f7d780f4fd9774115cf8c695a03
cooler-SAI <cooler19860212@yahoo.com>
2022-05-17 22:37:33 +0300

    DB/Wandering: enable move ANIMALS_BEASTS_BASILISK ,
    ANIMALS_BEASTS_BEAR,
    ANIMALS_BEASTS_BOAR

---
+++ SQL/updates/war_world/2022_05_17_00_war_world_wandering.sql
@@ -0,0 +1,6 @@
+-- ANIMALS_BEASTS_BASILISK 
+UPDATE `creature_protos` SET `IsWandering` = 1 WHERE `CreatureType` = 1 AND `CreatureSubType` = 1;
+-- ANIMALS_BEASTS_BEAR 
+UPDATE `creature_protos` SET `IsWandering` = 1 WHERE `CreatureType` = 1 AND `CreatureSubType` = 2;
+-- ANIMALS_BEASTS_BOAR 
+UPDATE `creature_protos` SET `IsWandering` = 1 WHERE `CreatureType` = 1 AND `CreatureSubType` = 3;
