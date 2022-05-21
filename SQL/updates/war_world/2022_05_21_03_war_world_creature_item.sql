c41ad17df53a0477cf66ffdcfde04f1486e96165
cooler-SAI <cooler19860212@yahoo.com>
2022-05-22 00:05:58 +0300

    DB/creature_item: fixed more effects

---
+++ SQL/updates/war_world/2022_05_21_03_war_world_creature_item.sql
@@ -0,0 +1,9 @@
+DELETE FROM `creature_items` WHERE `Entry`=18;
+INSERT  INTO `creature_items`(`Entry`,`SlotId`,`ModelId`,`EffectId`,`PrimaryColor`,`SecondaryColor`) values 
+(18,10,1473,0,0,0),
+(18,11,1372,0,0,0),
+(18,20,3094,8094,0,0),
+(18,21,3096,0,0,0),
+(18,22,3095,0,0,0),
+(18,25,3098,8094,0,0),
+(18,28,2488,0,0,0);
