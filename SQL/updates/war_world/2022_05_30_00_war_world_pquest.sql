843c376d9b3423f20403e9bb9a339941a9f3b318
cooler-SAI <cooler19860212@yahoo.com>
2022-05-30 14:03:39 +0300

    DB/PQuest: fix Raven Host Vanguard event sound, close https://github.com/Shmerrick/ProjectWAR/issues/70

---
+++ SQL/updates/war_world/2022_05_30_00_war_world_pquest.sql
@@ -0,0 +1,6 @@
+UPDATE `pquest_objectives` SET `SoundIteration` = '1' WHERE `Guid` = '755'; 
+UPDATE `pquest_objectives` SET `SoundIteration` = '1' WHERE `Guid` = '756'; 
+UPDATE `pquest_objectives` SET `SoundIteration` = '1' WHERE `Guid` = '757'; 
+UPDATE `pquest_objectives` SET `SoundDelay` = '1' WHERE `Guid` = '755'; 
+UPDATE `pquest_objectives` SET `SoundDelay` = '1' WHERE `Guid` = '756'; 
+UPDATE `pquest_objectives` SET `SoundDelay` = '1' WHERE `Guid` = '757'; 
