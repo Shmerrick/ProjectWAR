d26d9d1f5031810b0dead68f03144a1ac56b8b93
cooler-SAI <cooler19860212@yahoo.com>
2022-06-01 23:07:49 +0300

    Common/HonorCalculation: format fixes, other one

--- Common/HonorCalculation.cs
+++ Common/HonorCalculation.cs
@@ -5,12 +5,11 @@ namespace Common
 {
     public class HonorCalculation
     {
-        private Dictionary<int, int> honorLevelReference = new Dictionary<int, int>();
         public const int HONOR_RANK_1 = 1000;
         public const int HONOR_RANK_2 = 2000;
         public const int HONOR_RANK_3 = 4000;
         public const int HONOR_RANK_4 = 8000;
-
+        private readonly Dictionary<int, int> honorLevelReference = new Dictionary<int, int>();
         public HonorCalculation()
         {
             // This list MUST be descending in sequence for this to work ;)
@@ -18,6 +17,31 @@ namespace Common
             honorLevelReference.Add(3, HONOR_RANK_3);
             honorLevelReference.Add(2, HONOR_RANK_2);
             honorLevelReference.Add(1, HONOR_RANK_1);
+        }
+
+        public static int CalculateRank0Percent(ushort infoHonorPoints)
+        {
+            return RoundOffNearestTen((100 * infoHonorPoints) / HONOR_RANK_1);
+        }
+
+        public static int CalculateRank1Percent(ushort infoHonorPoints)
+        {
+            return RoundOffNearestTen((100 * (infoHonorPoints - HONOR_RANK_2 + HONOR_RANK_1)) / HONOR_RANK_1);
+        }
+
+        public static int CalculateRank2Percent(ushort infoHonorPoints)
+        {
+            return RoundOffNearestTen((100 * (infoHonorPoints - HONOR_RANK_3 + HONOR_RANK_2)) / HONOR_RANK_2);
+        }
+
+        public static int CalculateRank3Percent(ushort infoHonorPoints)
+        {
+            return RoundOffNearestTen((100 * (infoHonorPoints - HONOR_RANK_4 + HONOR_RANK_3)) / HONOR_RANK_3);
+        }
+
+        public static int RoundOffNearestTen(int i)
+        {
+            return ((int)Math.Round(i / 10.0)) * 10;
         }
 
         public int GetHonorLevel(int honorPoints)
@@ -28,31 +52,6 @@ namespace Common
                     return honorLevel.Key;
             }
             return 0;
-        }
-
-        public static int RoundOffNearestTen(int i)
-        {
-            return ((int)Math.Round(i / 10.0)) * 10;
-        }
-
-        public static int CalculateRank0Percent(ushort infoHonorPoints)
-        {
-            return RoundOffNearestTen((100 * infoHonorPoints) / HonorCalculation.HONOR_RANK_1);
-        }
-
-        public static int CalculateRank1Percent(ushort infoHonorPoints)
-        {
-            return RoundOffNearestTen((100 * (infoHonorPoints - HonorCalculation.HONOR_RANK_2 + HonorCalculation.HONOR_RANK_1)) / HonorCalculation.HONOR_RANK_1);
-        }
-
-        public static int CalculateRank2Percent(ushort infoHonorPoints)
-        {
-            return RoundOffNearestTen((100 * (infoHonorPoints - HonorCalculation.HONOR_RANK_3 + HonorCalculation.HONOR_RANK_2)) / HonorCalculation.HONOR_RANK_2);
-        }
-
-        public static int CalculateRank3Percent(ushort infoHonorPoints)
-        {
-            return RoundOffNearestTen((100 * (infoHonorPoints - HonorCalculation.HONOR_RANK_4 + HonorCalculation.HONOR_RANK_3)) / HonorCalculation.HONOR_RANK_3);
         }
     }
 }
