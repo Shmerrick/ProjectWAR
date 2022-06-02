bf3d50ba23f3ab6fe8b562857e23492276c4f767
cooler-SAI <cooler19860212@yahoo.com>
2022-06-02 10:36:08 +0300

    Common/DB_BuffInfo: reorganize format

--- Common/Database/World/Ability/DB_BuffInfo.cs
+++ Common/Database/World/Ability/DB_BuffInfo.cs
@@ -7,33 +7,50 @@ namespace Common
     [Serializable]
     public class DBBuffInfo : DataObject
     {
-        [PrimaryKey]
-        public ushort Entry { get; set; }
-
+        /// <summary>Indicates how an aura should propagate.</summary>
         [DataElement(Varchar = 255)]
-        public string Name { get; set; }
+        public string AuraPropagation { get; set; }
 
         [DataElement(Varchar = 8)]
         public string BuffClassString { get; set; }
 
-        [DataElement(Varchar = 11)]
-        public string TypeString { get; set; }
+        [DataElement]
+        public bool CanRefresh { get; set; }
+
+        [DataElement]
+        public uint Duration { get; set; }
+
+        [DataElement]
+        public byte EnemyEffectID { get; set; }
+
+        [PrimaryKey]
+        public ushort Entry { get; set; }
+
+        [DataElement]
+        public byte FriendlyEffectID { get; set; }
 
         [DataElement]
         public byte Group { get; set; }
 
-        /// <summary>Indicates how an aura should propagate.</summary>
-        [DataElement(Varchar = 255)]
-        public string AuraPropagation { get; set; }
+        [DataElement]
+        public ushort Interval { get; set; }
+
+        [DataElement]
+        public int LeadInDelay { get; set; }
 
         [DataElement]
         public byte MaxCopies { get; set; }
 
         [DataElement]
-        public bool UseMaxStackAsInitial { get; set; }
+        public byte MaxStack { get; set; }
+
+        [DataElement(Varchar = 255)]
+        public string Name { get; set; }
+        [DataElement]
+        public byte PersistsOnDeath { get; set; }
 
         [DataElement]
-        public byte MaxStack { get; set; }
+        public byte Silent { get; set; }
 
         [DataElement]
         public byte StackLine { get; set; }
@@ -41,28 +58,9 @@ namespace Common
         [DataElement]
         public bool StacksFromCaster { get; set; }
 
+        [DataElement(Varchar = 11)]
+        public string TypeString { get; set; }
         [DataElement]
-        public uint Duration { get; set; }
-
-        [DataElement]
-        public int LeadInDelay { get; set; }
-
-        [DataElement]
-        public ushort Interval { get; set; }
-
-        [DataElement]
-        public byte PersistsOnDeath { get; set; }
-
-        [DataElement]
-        public bool CanRefresh { get; set; }
-
-        [DataElement]
-        public byte FriendlyEffectID { get; set; }
-
-        [DataElement]
-        public byte EnemyEffectID { get; set; }
-
-        [DataElement]
-        public byte Silent { get; set; }
+        public bool UseMaxStackAsInitial { get; set; }
     }
 }
