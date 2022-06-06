ddbe3bedfba785291a2566850f3b47ee5454382f
cooler-SAI <cooler19860212@yahoo.com>
2022-06-06 20:49:18 +0300

    Revert "Common/Characters: edit format, fix style"
    This reverts commit 5fd0a5e8c91a9c4f28c077cd00ee8ac0edb65c50.

--- Common/Database/Character/Characters.cs
+++ Common/Database/Character/Characters.cs
@@ -11,42 +11,59 @@ namespace Common
     [Serializable]
     public class Character : DataObject
     {
-        public List<Characters_bag_pools> Bag_Pools;
-        public List<CharacterSavedBuff> Buffs;
-        public CharacterClientData ClientData;
-        public bool FirstConnect;
-        public List<Characters_influence> Influences;
-        public List<Character_mail> Mails;
-        public List<Character_quest> Quests;
-        public List<Character_social> Socials;
-        public string TempFirstName;
-        //name to be shown to everyone (used for world events)
-        public string TempLastName;
-
-        public List<Character_tok_kills> TokKills;
-        public List<Character_tok> Toks;
-        public Character_value Value;
+        private uint _characterId;
+        private string _name;
+        private string _surname;
+        private int _realmId;
         private int _accountId;
-        private bool _anonymous;
+        private byte _slotId;
+        private byte _modelId;
         private byte _career;
         private byte _careerLine;
-        private uint _characterId;
+        private byte _realm;
         private int _heldLeft;
+        private byte _race;
+        private byte[] _traits;
+        private byte _sex;
+        public bool FirstConnect;
+        private bool _anonymous;
         private bool _hidden;
+        private string _oldName;
+        private string _petName;
+        private ushort _petModel;
         private ushort _honorPoints;
         private ushort _honorRank;
-        private byte _modelId;
-        private string _name;
-        private string _oldName;
-        private ushort _petModel;
-        private string _petName;
-        private byte _race;
-        private byte _realm;
-        private int _realmId;
-        private byte _sex;
-        private byte _slotId;
-        private string _surname;
-        private byte[] _traits;
+
+        public uint CareerFlags => CareerLine != 0 ? (uint)1 << (_careerLine - 1) : 0;
+
+        [PrimaryKey]
+        public uint CharacterId
+        {
+            get { return _characterId; }
+            set { _characterId = value; Dirty = true; }
+        }
+
+        [DataElement(Unique = true, AllowDbNull = false, Varchar = 24)]
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false, Varchar = 24)]
+        public string Surname
+        {
+            get { return _surname; }
+            set { _surname = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false)]
+        public int RealmId
+        {
+            get { return _realmId; }
+            set { _realmId = value; Dirty = true; }
+        }
+
         [DataElement(AllowDbNull = false)]
         public int AccountId
         {
@@ -55,16 +72,17 @@ namespace Common
         }
 
         [DataElement(AllowDbNull = false)]
-        public bool Anonymous
+        public byte SlotId
         {
-            get { return _anonymous; }
-            set { _anonymous = value; Dirty = true; }
+            get { return _slotId; }
+            set { _slotId = value; Dirty = true; }
         }
 
-        public byte[] bTraits
+        [DataElement(AllowDbNull = false)]
+        public byte ModelId
         {
-            get { return _traits; }
-            set { _traits = value; Dirty = true; }
+            get { return _modelId; }
+            set { _modelId = value; Dirty = true; }
         }
 
         [DataElement(AllowDbNull = false)]
@@ -74,8 +92,6 @@ namespace Common
             set { _career = value; Dirty = true; }
         }
 
-        public uint CareerFlags => CareerLine != 0 ? (uint)1 << (_careerLine - 1) : 0;
-
         [DataElement(AllowDbNull = false)]
         public byte CareerLine
         {
@@ -83,11 +99,11 @@ namespace Common
             set { _careerLine = value; Dirty = true; }
         }
 
-        [PrimaryKey]
-        public uint CharacterId
+        [DataElement(AllowDbNull = false)]
+        public byte Realm
         {
-            get { return _characterId; }
-            set { _characterId = value; Dirty = true; }
+            get { return _realm; }
+            set { _realm = value; Dirty = true; }
         }
 
         [DataElement(AllowDbNull = false)]
@@ -98,13 +114,69 @@ namespace Common
         }
 
         [DataElement(AllowDbNull = false)]
+        public byte Race
+        {
+            get { return _race; }
+            set { _race = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false)]
+        public string Traits
+        {
+            get { return Encoding.UTF8.GetString(_traits); }
+            set
+            {
+                _traits = Encoding.UTF8.GetBytes(value); Dirty = true;
+            }
+        }
+
+        [DataElement(AllowDbNull = false)]
+        public byte Sex
+        {
+            get { return _sex; }
+            set { _sex = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false)]
+        public bool Anonymous
+        {
+            get { return _anonymous; }
+            set { _anonymous = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false)]
         public bool Hidden
         {
             get { return _hidden; }
             set { _hidden = value; Dirty = true; }
         }
 
-        public List<HonorRewardCooldown> HonorCooldowns { get; set; }
+        public byte[] bTraits
+        {
+            get { return _traits; }
+            set { _traits = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false, Varchar = 24)]
+        public string OldName
+        {
+            get { return _oldName; }
+            set { _oldName = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false, Varchar = 24)]
+        public string PetName
+        {
+            get { return _petName; }
+            set { _petName = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false)]
+        public ushort PetModel
+        {
+            get { return _petModel; }
+            set { _petModel = value; Dirty = true; }
+        }
 
         [DataElement(AllowDbNull = false)]
         public ushort HonorPoints
@@ -120,91 +192,29 @@ namespace Common
             set { _honorRank = value; Dirty = true; }
         }
 
-        [DataElement(AllowDbNull = false)]
-        public byte ModelId
-        {
-            get { return _modelId; }
-            set { _modelId = value; Dirty = true; }
-        }
+        public List<HonorRewardCooldown> HonorCooldowns { get; set; }
 
-        [DataElement(Unique = true, AllowDbNull = false, Varchar = 24)]
-        public string Name
-        {
-            get { return _name; }
-            set { _name = value; Dirty = true; }
-        }
+        public Character_value Value;
 
-        [DataElement(AllowDbNull = false, Varchar = 24)]
-        public string OldName
-        {
-            get { return _oldName; }
-            set { _oldName = value; Dirty = true; }
-        }
+        public CharacterClientData ClientData;
 
-        [DataElement(AllowDbNull = false)]
-        public ushort PetModel
-        {
-            get { return _petModel; }
-            set { _petModel = value; Dirty = true; }
-        }
+        public List<Character_social> Socials;
 
-        [DataElement(AllowDbNull = false, Varchar = 24)]
-        public string PetName
-        {
-            get { return _petName; }
-            set { _petName = value; Dirty = true; }
-        }
+        public List<Character_tok> Toks;
 
-        [DataElement(AllowDbNull = false)]
-        public byte Race
-        {
-            get { return _race; }
-            set { _race = value; Dirty = true; }
-        }
+        public List<Character_tok_kills> TokKills;
 
-        [DataElement(AllowDbNull = false)]
-        public byte Realm
-        {
-            get { return _realm; }
-            set { _realm = value; Dirty = true; }
-        }
+        public List<Character_quest> Quests;
 
-        [DataElement(AllowDbNull = false)]
-        public int RealmId
-        {
-            get { return _realmId; }
-            set { _realmId = value; Dirty = true; }
-        }
+        public List<Characters_influence> Influences;
 
-        [DataElement(AllowDbNull = false)]
-        public byte Sex
-        {
-            get { return _sex; }
-            set { _sex = value; Dirty = true; }
-        }
+        public List<Characters_bag_pools> Bag_Pools;
 
-        [DataElement(AllowDbNull = false)]
-        public byte SlotId
-        {
-            get { return _slotId; }
-            set { _slotId = value; Dirty = true; }
-        }
+        public List<Character_mail> Mails;
 
-        [DataElement(AllowDbNull = false, Varchar = 24)]
-        public string Surname
-        {
-            get { return _surname; }
-            set { _surname = value; Dirty = true; }
-        }
-        [DataElement(AllowDbNull = false)]
-        public string Traits
-        {
-            get { return Encoding.UTF8.GetString(_traits); }
-            set
-            {
-                _traits = Encoding.UTF8.GetBytes(value); Dirty = true;
-            }
-        }
- //(used for world events)
+        public List<CharacterSavedBuff> Buffs;
+
+        public string TempFirstName; //name to be shown to everyone (used for world events)
+        public string TempLastName; //(used for world events)
     }
 }
