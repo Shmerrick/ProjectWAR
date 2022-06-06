6dc7a00296025610da4ea67416c2ce682c641ed5
cooler-SAI <cooler19860212@yahoo.com>
2022-06-06 20:43:53 +0300

    Revert "Common/AbilityModifierEffects: fix format"
    This reverts commit 9db67dae90b0c5fb3729b85922f0384df9a56dd8.

--- Common/Database/World/Ability/AbilityModifierEffects.cs
+++ Common/Database/World/Ability/AbilityModifierEffects.cs
@@ -9,23 +9,39 @@ namespace Common
     {
         public AbilityModifierEffect nextMod;
 
+        public void AddModifier(AbilityModifierEffect newMod)
+        {
+            if (nextMod != null)
+                nextMod.AddModifier(newMod);
+            else nextMod = newMod;
+        }
+
+        [DataElement()]
+        public ushort Entry { get; set; }
+
         [DataElement(Varchar = 48)]
-        public string AffectedAbility { get; set; }
+        public string SourceAbility { get; set; }
 
         [DataElement()]
         public ushort Affecting { get; set; }
 
-        [DataElement()]
-        public byte BuffLine { get; set; }
+        [DataElement(Varchar = 48)]
+        public string AffectedAbility { get; set; }
 
         [DataElement()]
-        public ushort Entry { get; set; }
+        public byte PreOrPost { get; set; }
+
+        [DataElement()]
+        public byte Sequence { get; set; }
 
         [DataElement(Varchar = 255)]
         public string ModifierCommandName { get; set; }
 
         [DataElement()]
-        public byte PreOrPost { get; set; }
+        public byte TargetCommandID { get; set; }
+
+        [DataElement()]
+        public byte TargetCommandSequence { get; set; }
 
         [DataElement()]
         public int PrimaryValue { get; set; }
@@ -34,22 +50,6 @@ namespace Common
         public int SecondaryValue { get; set; }
 
         [DataElement()]
-        public byte Sequence { get; set; }
-
-        [DataElement(Varchar = 48)]
-        public string SourceAbility { get; set; }
-
-        [DataElement()]
-        public byte TargetCommandID { get; set; }
-
-        [DataElement()]
-        public byte TargetCommandSequence { get; set; }
-
-        public void AddModifier(AbilityModifierEffect newMod)
-        {
-            if (nextMod != null)
-                nextMod.AddModifier(newMod);
-            else nextMod = newMod;
-        }
+        public byte BuffLine { get; set; }
     }
 }
