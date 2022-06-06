f9325dc397bbe961b6ebf67a44a6225e7407e025
cooler-SAI <cooler19860212@yahoo.com>
2022-06-06 20:45:50 +0300

    Revert "Common/Guild_info: reorganize file format"
    This reverts commit 5218be6e4474625deec1ac0b749d2c7c5f2d6995.

--- Common/Database/Character/Guild_info.cs
+++ Common/Database/Character/Guild_info.cs
@@ -9,49 +9,87 @@ namespace Common
     [Serializable]
     public class Guild_info : DataObject
     {
-        public Dictionary<byte, Guild_event> Event = new Dictionary<byte, Guild_event>();
-        public List<Guild_log> Logs;
-        public Dictionary<uint, Guild_member> Members;
-        public Dictionary<byte, Guild_rank> Ranks;
-        public Dictionary<ushort, GuildVaultItem>[] Vaults =
-        {
-            new Dictionary<ushort, GuildVaultItem>(),
-            new Dictionary<ushort, GuildVaultItem>(),
-            new Dictionary<ushort, GuildVaultItem>(),
-            new Dictionary<ushort, GuildVaultItem>(),
-            new Dictionary<ushort, GuildVaultItem>()
-        };
-
+        private uint _GuildId;
+        private string _Name;
+        private byte _Level;
+        private byte _Realm;
+        private uint _LeaderId;
+        private int _CreateDate;
+        private string _Motd;
         private string _AboutUs;
-        private byte _ActivelyRecruiting;
-        private uint _AllianceId;
-        private byte _Atmosphere;
+        private uint _Xp;
+        private ulong _Renown;
+        private byte _Tax;
+        private ulong _Money;
+        private byte[] _guildvaultpurchased;
+        private string _Heraldry;
         private string _Banners;
+        private ushort[] _GuildTacticsPurchased;
+
         // Used on recruitment
         private string _BriefDescription;
 
-        private uint _CareersNeeded;
-        private int _CreateDate;
-        private uint _GuildId;
-        private ushort[] _GuildTacticsPurchased;
-        private byte[] _guildvaultpurchased;
-        private string _Heraldry;
-        private byte _Interests;
-        private uint _LeaderId;
-        private byte _Level;
-        private ulong _Money;
-        private string _Motd;
-        private string _Name;
-        private byte _PlayStyle;
-        private byte _RanksNeeded;
-        private byte _Realm;
-        private ulong _Renown;
         private string _Summary;
-        private byte _Tax;
-        private uint _Xp;
+        private byte _PlayStyle;
+        private byte _Atmosphere;
+        private uint _CareersNeeded;
+        private byte _Interests;
+        private byte _ActivelyRecruiting;
+        private byte _RanksNeeded;
+        private uint _AllianceId;
+
         public Guild_info()
             : base()
         {
+        }
+
+        [PrimaryKey]
+        public uint GuildId
+        {
+            get { return _GuildId; }
+            set { _GuildId = value; Dirty = true; }
+        }
+
+        [DataElement(Unique = true, AllowDbNull = false, Varchar = 255)]
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false)]
+        public byte Level
+        {
+            get { return _Level; }
+            set { _Level = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false)]
+        public byte Realm
+        {
+            get { return _Realm; }
+            set { _Realm = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false)]
+        public uint LeaderId
+        {
+            get { return _LeaderId; }
+            set { _LeaderId = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false)]
+        public int CreateDate
+        {
+            get { return _CreateDate; }
+            set { _CreateDate = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false)]
+        public string Motd
+        {
+            get { return _Motd; }
+            set { _Motd = value; Dirty = true; }
         }
 
         [DataElement(AllowDbNull = false)]
@@ -62,31 +100,17 @@ namespace Common
         }
 
         [DataElement(AllowDbNull = false)]
-        public byte ActivelyRecruiting
+        public uint Xp
         {
-            get { return _ActivelyRecruiting; }
-            set { _ActivelyRecruiting = value; Dirty = true; }
-        }
-
-        [DataElement(AllowDbNull = true)]
-        public uint AllianceId
-        {
-            get { return _AllianceId; }
-            set { _AllianceId = value; Dirty = true; }
+            get { return _Xp; }
+            set { _Xp = value; Dirty = true; }
         }
 
         [DataElement(AllowDbNull = false)]
-        public byte Atmosphere
+        public ulong Renown
         {
-            get { return _Atmosphere; }
-            set { _Atmosphere = value; Dirty = true; }
-        }
-
-        [DataElement(AllowDbNull = false)]
-        public string Banners
-        {
-            get { return _Banners; }
-            set { _Banners = value; Dirty = true; }
+            get { return _Renown; }
+            set { _Renown = value; Dirty = true; }
         }
 
         [DataElement(AllowDbNull = false)]
@@ -97,6 +121,27 @@ namespace Common
         }
 
         [DataElement(AllowDbNull = false)]
+        public string Summary
+        {
+            get { return _Summary; }
+            set { _Summary = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false)]
+        public byte PlayStyle
+        {
+            get { return _PlayStyle; }
+            set { _PlayStyle = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false)]
+        public byte Atmosphere
+        {
+            get { return _Atmosphere; }
+            set { _Atmosphere = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false)]
         public uint CareersNeeded
         {
             get { return _CareersNeeded; }
@@ -104,39 +149,38 @@ namespace Common
         }
 
         [DataElement(AllowDbNull = false)]
-        public int CreateDate
+        public byte Interests
         {
-            get { return _CreateDate; }
-            set { _CreateDate = value; Dirty = true; }
-        }
-
-        [PrimaryKey]
-        public uint GuildId
-        {
-            get { return _GuildId; }
-            set { _GuildId = value; Dirty = true; }
+            get { return _Interests; }
+            set { _Interests = value; Dirty = true; }
         }
 
         [DataElement(AllowDbNull = false)]
-        public ushort[] GuildTacticsPurchased
+        public byte ActivelyRecruiting
         {
-            get
-            {
-                if (_GuildTacticsPurchased == null || _GuildTacticsPurchased.Length < 5)
-                {
-                    _GuildTacticsPurchased = new ushort[40];
-                }
-                return _GuildTacticsPurchased;
-            }
-            set
-            {
-                _GuildTacticsPurchased = value;
-                if (_GuildTacticsPurchased == null || _GuildTacticsPurchased.Length < 5)
-                {
-                    _GuildTacticsPurchased = new ushort[40];
-                }
-                Dirty = true;
-            }
+            get { return _ActivelyRecruiting; }
+            set { _ActivelyRecruiting = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false)]
+        public byte RanksNeeded
+        {
+            get { return _RanksNeeded; }
+            set { _RanksNeeded = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false)]
+        public byte Tax
+        {
+            get { return _Tax; }
+            set { _Tax = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false)]
+        public ulong Money
+        {
+            get { return _Money; }
+            set { _Money = value; Dirty = true; }
         }
 
         [DataElement(AllowDbNull = false)]
@@ -162,6 +206,13 @@ namespace Common
         }
 
         [DataElement(AllowDbNull = false)]
+        public string Banners
+        {
+            get { return _Banners; }
+            set { _Banners = value; Dirty = true; }
+        }
+
+        [DataElement(AllowDbNull = false)]
         public string Heraldry
         {
             get { return _Heraldry; }
@@ -169,92 +220,46 @@ namespace Common
         }
 
         [DataElement(AllowDbNull = false)]
-        public byte Interests
+        public ushort[] GuildTacticsPurchased
         {
-            get { return _Interests; }
-            set { _Interests = value; Dirty = true; }
+            get
+            {
+                if (_GuildTacticsPurchased == null || _GuildTacticsPurchased.Length < 5)
+                {
+                    _GuildTacticsPurchased = new ushort[40];
+                }
+                return _GuildTacticsPurchased;
+            }
+            set
+            {
+                _GuildTacticsPurchased = value;
+                if (_GuildTacticsPurchased == null || _GuildTacticsPurchased.Length < 5)
+                {
+                    _GuildTacticsPurchased = new ushort[40];
+                }
+                Dirty = true;
+            }
         }
 
-        [DataElement(AllowDbNull = false)]
-        public uint LeaderId
+        [DataElement(AllowDbNull = true)]
+        public uint AllianceId
         {
-            get { return _LeaderId; }
-            set { _LeaderId = value; Dirty = true; }
+            get { return _AllianceId; }
+            set { _AllianceId = value; Dirty = true; }
         }
 
-        [DataElement(AllowDbNull = false)]
-        public byte Level
+        public Dictionary<ushort, GuildVaultItem>[] Vaults =
         {
-            get { return _Level; }
-            set { _Level = value; Dirty = true; }
-        }
+            new Dictionary<ushort, GuildVaultItem>(),
+            new Dictionary<ushort, GuildVaultItem>(),
+            new Dictionary<ushort, GuildVaultItem>(),
+            new Dictionary<ushort, GuildVaultItem>(),
+            new Dictionary<ushort, GuildVaultItem>()
+        };
 
-        [DataElement(AllowDbNull = false)]
-        public ulong Money
-        {
-            get { return _Money; }
-            set { _Money = value; Dirty = true; }
-        }
-
-        [DataElement(AllowDbNull = false)]
-        public string Motd
-        {
-            get { return _Motd; }
-            set { _Motd = value; Dirty = true; }
-        }
-
-        [DataElement(Unique = true, AllowDbNull = false, Varchar = 255)]
-        public string Name
-        {
-            get { return _Name; }
-            set { _Name = value; Dirty = true; }
-        }
-        [DataElement(AllowDbNull = false)]
-        public byte PlayStyle
-        {
-            get { return _PlayStyle; }
-            set { _PlayStyle = value; Dirty = true; }
-        }
-
-        [DataElement(AllowDbNull = false)]
-        public byte RanksNeeded
-        {
-            get { return _RanksNeeded; }
-            set { _RanksNeeded = value; Dirty = true; }
-        }
-
-        [DataElement(AllowDbNull = false)]
-        public byte Realm
-        {
-            get { return _Realm; }
-            set { _Realm = value; Dirty = true; }
-        }
-        [DataElement(AllowDbNull = false)]
-        public ulong Renown
-        {
-            get { return _Renown; }
-            set { _Renown = value; Dirty = true; }
-        }
-
-        [DataElement(AllowDbNull = false)]
-        public string Summary
-        {
-            get { return _Summary; }
-            set { _Summary = value; Dirty = true; }
-        }
-
-        [DataElement(AllowDbNull = false)]
-        public byte Tax
-        {
-            get { return _Tax; }
-            set { _Tax = value; Dirty = true; }
-        }
-
-        [DataElement(AllowDbNull = false)]
-        public uint Xp
-        {
-            get { return _Xp; }
-            set { _Xp = value; Dirty = true; }
-        }
+        public Dictionary<uint, Guild_member> Members;
+        public Dictionary<byte, Guild_rank> Ranks;
+        public Dictionary<byte, Guild_event> Event = new Dictionary<byte, Guild_event>();
+        public List<Guild_log> Logs;
     }
 }
