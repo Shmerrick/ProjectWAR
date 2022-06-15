55d68fc50ef2bbebde1296fbe96f28aa4d1447ca
cooler-SAI <cooler19860212@yahoo.com>
2022-06-15 23:50:49 +0300

    DB/creature_protos: added Wandering for ANIMALS_BEASTS_HOUND and ANIMALS_BEASTS_RHINOX

---
+++ SQL/updates/war_world/2022_06_15_00_war_world_wandering.sql
@@ -0,0 +1,4 @@
+-- ANIMALS_BEASTS_HOUND
+UPDATE `creature_protos` SET `IsWandering` = 1 WHERE `CreatureType` = 1 AND `CreatureSubType` = 6;
+-- ANIMALS_BEASTS_RHINOX
+UPDATE `creature_protos` SET `IsWandering` = 1 WHERE `CreatureType` = 1 AND `CreatureSubType` = 7;
