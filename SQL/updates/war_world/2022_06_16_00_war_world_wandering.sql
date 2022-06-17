f23d696dc9f6d1859b7e3ed9c0d7f04fd310d5fc
cooler-SAI <cooler19860212@yahoo.com>
2022-06-17 21:48:31 +0300

    Revert "Creature/Wandering: update state wander for all creatures"
    This reverts commit 15af0fcfe86b7341b8f551f4b5a8fb5b18f5fc63.

--- SQL/updates/war_world/2022_06_16_00_war_world_wandering.sql
+++
@@ -1,2 +0,0 @@
-UPDATE `creature_protos` SET `IsWandering` = '1' WHERE (`MinLevel`<= '43' AND `MaxLevel`<= '43' AND `Unk2`<= '1001' AND `CreatureType` != '32' AND `VendorID`= '0'
-AND `LairBoss`= '0' AND `Title`= '0' AND `Title`= '0' AND `Emote`= '0');
