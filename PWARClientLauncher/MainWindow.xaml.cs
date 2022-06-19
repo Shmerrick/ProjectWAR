ed30121db8f3e721ce95639cb59a870c5d4c6a9d
cooler-SAI <cooler19860212@yahoo.com>
2022-06-19 15:23:49 +0300

    PWARClientLauncher/Project: added new one here, only base functions at this moment

---
+++ PWARClientLauncher/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PWARClientLauncher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly CreateAccount createAccount;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            createAccount = new CreateAccount();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_CreateAccount_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
