1e5d325c5ee7ed0d8b268b7d7223c153a132296e
cooler-SAI <cooler19860212@yahoo.com>
2022-08-04 18:06:32 +0300

    delete old data

--- PWARAbilityTool/Dtos/KillTracker.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PWARAbilityTool
-{
-    public class KillTracker
-    {
-        public int CharacterId { get; set; }
-        public int KillCount { get; set; }
-        public string CharacterName { get; set; }
-    }
-}
