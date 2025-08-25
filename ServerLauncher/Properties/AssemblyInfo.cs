dbb786e5b581746fa6686e0b8c701af8b8811dce
Shmerrick <33600225+Shmerrick@users.noreply.github.com>
2025-08-25 13:52:38 -0400

    Remove assembly attributes causing duplicates

--- ServerLauncher/Properties/AssemblyInfo.cs
+++ ServerLauncher/Properties/AssemblyInfo.cs
@@ -6,23 +6,13 @@ using System.Runtime.InteropServices;
 // the following set of attributes. Change the values ​​of these attributes to change the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("ServerLauncher")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ServerLauncher")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types invisible in this assembly
 // to COM components. If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on this type.
 
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9f04d359-dd88-4c89-8167-8c61fb91ed1d")]
 
 // The version information for an assembly consists of the following four values:
 //
@@ -35,5 +25,3 @@ using System.Runtime.InteropServices;
 // using '*', as shown below:
 // [assembly: AssemblyVersion ("1.0. *")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
